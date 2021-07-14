11c1b9a875ea6fae3084d56a8018fcef7329e0e9
Hatice <haticekaraty@gmail.com>
2021-07-14 19:16:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let ulTasks = document.querySelector("ul#tasks");
+  let form = document.getElementById("create-task-form");
+  
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let input = document.getElementById("new-task-description");
+    let taskFromInput = document.getElementById("new-task-description").value;
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerHTML = "X";
+    li.innerText = taskFromInput;
+    li.appendChild(button);
+    ulTasks.appendChild(li);
+    input.value = "";
+    button.addEventListener("click",handleDelete);
+  });
 });
+
+function handleDelete(e){
+  e.target.parentElement.remove();
+}
+
+
